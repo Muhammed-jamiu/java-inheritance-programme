0bf128166b821a6c788e6c642c7f9bf9e7fbef2f
Anate Muhammed <anatemuhammed@gmail.com>
2024-09-17 14:00:50 +0100

    first commit

---
+++ Inheritance.java
@@ -0,0 +1,17 @@
+ public class Inheritance{
+    private boolean isEnable= true;
+
+    public void enable(){
+        isEnable= true;
+    }
+
+
+    public void disable(){
+        isEnable= false;
+    }
+
+//get section
+     public boolean isEnable() {
+         return isEnable;
+     }
+ }
---
+++ MainInheritance.java
@@ -0,0 +1,7 @@
+public class MainInheritance {
+    public static void main(String[] args){
+        var inheritance= new Inheritance();
+        inheritance.enable();
+        System.out.println(inheritance.isEnable());
+    }
+}
